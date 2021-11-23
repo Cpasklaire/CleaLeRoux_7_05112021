1edb478688023e5c2890b24fab204dc00b248a23
Cléa <80284454+Cpasklaire@users.noreply.github.com>
2021-11-23 05:15:30 +0100

    Create Connect.vue

---
+++ src/views/Connect.vue
@@ -0,0 +1,38 @@
+<template v-if="!token">
+    <section class="connect">
+        <form class="login">
+            <label>Couriel</label>
+            <input type="email" required/>
+            <label>Mot de passe</label>
+            <input type="password" required/>
+            <button type="submit">Connection</button>
+        </form>
+        <form class="signin">
+            <label>Nom</label>
+            <input type="text" required placeholder="Phenix"/>
+            <label>Prénom</label>
+            <input type="text" required placeholder="Ikki"/>
+            <label>Couriel</label>
+            <input required type="email" pattern=".+@groupamania.fr" placeholder="ikkiphenix@groupamania.fr"/>
+            <label>Mot de passe</label>
+            <input type="password" required/>
+            <button type="submit">S'inscrire</button>
+        </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Connect',
+        components: {},
+        data:function(){
+            return{
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
